b735c4cd7744d15b2ffc38417d03b158cd6b6061
kika <anamonteiro430@gmail.com>
2020-05-18 23:57:25 +0100

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +9,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
 
-    # Your code here
+    while left <= right:
+        mid = left + right // 2
 
+        #Check if target is at mid
+        if arr[mid] == target:
+            return mid
+        #If target is greater, ignore left half
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
     return -1  # not found
---
+++ test.py
@@ -0,0 +1,40 @@
+""" Print out each element of the following array on a separate line:
+['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+You may use whatever programming language you'd like.
+Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process. """
+
+
+""" Print out each element of the following array on a separate line, but this time the input array can contain arrays nested to an arbitrarily deep level:
+['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+For the above input, the expected output is:
+Bob
+Slack
+reddit
+89
+101
+alacritty
+(brackets)
+5
+375
+0
+{slice, owned}
+22
+You may use whatever programming language you'd like.
+Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process. """
+
+arr = ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+
+def teste(array):
+     for i in arr:
+          print(i)
+
+
+def teste1(array):
+     for i in arr:
+     
+          print(i)
+     for j in i:
+          print(j)
+
+
+teste(arr)
